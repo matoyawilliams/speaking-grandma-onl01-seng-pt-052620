8b6e762484bdcdea60daa3ca8e1b2b18096e25c7
Matoya Williams <matoyawilliams@yahoo.com>
2020-06-04 20:00:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(string)
   
-  if string == string.upcase
+if string == string.upcase
     puts "NO, NOT SINCE 1938!"
   elsif string == string.downcase
     puts "HUH?! SPEAK UP, SONNY!"
